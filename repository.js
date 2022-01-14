dd9c1c2f261177e45e9f05a7252c1fe5ff1f6f57
SimaTolstikova <94735694+SimaTolstikova@users.noreply.github.com>
2022-01-14 19:59:00 +0300

    Update repository.js

--- repository.js
+++ repository.js
@@ -1,26 +1,29 @@
-const { readFileSync, stat, writeFileSync, writeFile } = require('fs')
+const { readFileSync, stat, writeFileSync, writeFile } = require('fs')    //импортирует несколько методов из библиотеки, чтобы можно было ими пользоваться
 
 module.exports = new function () {
     const fileName = "./data.json";
     let inc = 0
     let data = {}
-    this.create = dt => {
-        dt.Id = inc++;
-        data[dt.Id] = dt;
+    this.create = dt => {   //создание id студента 
+        dt.Id = inc++;   //записывает id + 1
+        data[dt.Id] = dt;  //записывает инфу о новом студенте с общего списка
         writeFile(fileName, JSON.stringify(data), err => {if (err) console.error(err);});
-        return dt
+        return dt  //возвращает dt 
     }
+	
     this.getAll = () => {
-        return Object.values(data);
+        return Object.values(data);    //возвращаем инфу в базе 
     }
-    this.get = id => data[id];
-    this.update = dt => {
-        data[dt.Id] = dt;
+    this.get = id => data[id];    //записывавет id студента
+	
+    this.update = dt => {  //в dt есть id, по нему понимает, куда нужно положить новую информацию о студенте
+        data[dt.Id] = dt;   //находит
         writeFile(fileName, JSON.stringify(data), err => {if (err) console.error(err);});
-        return dt;
+        return dt;   //возвращает dt студента
     }
-    this.delete = id => {
-        delete data[id];
+	
+    this.delete = id => {   //получает id студента, которого нужно удалить
+        delete data[id];   //удаляет его
         writeFile(fileName, JSON.stringify(data), err => {if (err) console.error(err);});
     }
 
